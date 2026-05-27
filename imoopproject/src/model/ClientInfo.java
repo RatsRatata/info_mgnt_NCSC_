869939a3786c69f1ae124e2ed2c3922d638f03e9
Yoriib <domingorodolfo0934@gmail.com>
2026-05-27 18:40:24 +0800

    Testing some stuff, added getters and setters on ClientInfo.java

---
+++ imoopproject/src/model/ClientInfo.java
@@ -0,0 +1,159 @@
+package model;
+
+public class ClientInfo {
+    private String referenceCode;
+    private String name;
+    private String address;
+    private java.sql.Date birthDate;
+    private String birthPlace;
+    private char maritalStatus;
+    private char sex;
+    private String contactNumber;
+    private String emailAddress;
+    private String religion;
+    private String ethnicity;
+    private String languageSpoken;
+    private String oscaIdNum;
+    private String gsisSssNumber;
+    private String tinNum;
+    private String philhealthNum;
+    private String scAssociationId;
+    private String otherGovId;
+    private char travelCapability;
+    private String job;
+    private String currentPension;
+    private String highestEducationalAttainment;
+	public String getReferenceCode() {
+		return referenceCode;
+	}
+	public void setReferenceCode(String referenceCode) {
+		this.referenceCode = referenceCode;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public java.sql.Date getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(java.sql.Date birthDate) {
+		this.birthDate = birthDate;
+	}
+	public String getBirthPlace() {
+		return birthPlace;
+	}
+	public void setBirthPlace(String birthPlace) {
+		this.birthPlace = birthPlace;
+	}
+	public char getMaritalStatus() {
+		return maritalStatus;
+	}
+	public void setMaritalStatus(char maritalStatus) {
+		this.maritalStatus = maritalStatus;
+	}
+	public char getSex() {
+		return sex;
+	}
+	public void setSex(char sex) {
+		this.sex = sex;
+	}
+	public String getContactNumber() {
+		return contactNumber;
+	}
+	public void setContactNumber(String contactNumber) {
+		this.contactNumber = contactNumber;
+	}
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+	public String getReligion() {
+		return religion;
+	}
+	public void setReligion(String religion) {
+		this.religion = religion;
+	}
+	public String getEthnicity() {
+		return ethnicity;
+	}
+	public void setEthnicity(String ethnicity) {
+		this.ethnicity = ethnicity;
+	}
+	public String getLanguageSpoken() {
+		return languageSpoken;
+	}
+	public void setLanguageSpoken(String languageSpoken) {
+		this.languageSpoken = languageSpoken;
+	}
+	public String getOscaIdNum() {
+		return oscaIdNum;
+	}
+	public void setOscaIdNum(String oscaIdNum) {
+		this.oscaIdNum = oscaIdNum;
+	}
+	public String getGsisSssNumber() {
+		return gsisSssNumber;
+	}
+	public void setGsisSssNumber(String gsisSssNumber) {
+		this.gsisSssNumber = gsisSssNumber;
+	}
+	public String getTinNum() {
+		return tinNum;
+	}
+	public void setTinNum(String tinNum) {
+		this.tinNum = tinNum;
+	}
+	public String getPhilhealthNum() {
+		return philhealthNum;
+	}
+	public void setPhilhealthNum(String philhealthNum) {
+		this.philhealthNum = philhealthNum;
+	}
+	public String getScAssociationId() {
+		return scAssociationId;
+	}
+	public void setScAssociationId(String scAssociationId) {
+		this.scAssociationId = scAssociationId;
+	}
+	public String getOtherGovId() {
+		return otherGovId;
+	}
+	public void setOtherGovId(String otherGovId) {
+		this.otherGovId = otherGovId;
+	}
+	public char getTravelCapability() {
+		return travelCapability;
+	}
+	public void setTravelCapability(char travelCapability) {
+		this.travelCapability = travelCapability;
+	}
+	public String getJob() {
+		return job;
+	}
+	public void setJob(String job) {
+		this.job = job;
+	}
+	public String getCurrentPension() {
+		return currentPension;
+	}
+	public void setCurrentPension(String currentPension) {
+		this.currentPension = currentPension;
+	}
+	public String getHighestEducationalAttainment() {
+		return highestEducationalAttainment;
+	}
+	public void setHighestEducationalAttainment(String highestEducationalAttainment) {
+		this.highestEducationalAttainment = highestEducationalAttainment;
+	}
+    
+}
